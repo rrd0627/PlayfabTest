21d0d481d7c697eff6fdf01010bbebdbf9ac1b69
rrd0627 <39178521+rrd0627@users.noreply.github.com>
2020-09-20 01:43:39 +0900

    Add files via upload

---
+++ PlayerMove.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+public class PlayerMove : MonoBehaviour
+{
+    public PhotonView PV;
+
+    public SpriteRenderer SR;
+
+    private float Axis;
+    private void Start()
+    {
+        SR = GetComponent<SpriteRenderer>();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (PV.IsMine)
+        {
+            Axis = Input.GetAxisRaw("Horizontal");
+
+            transform.Translate(Axis * Time.deltaTime * 7, 0, 0);
+
+            if (Axis != 0) PV.RPC("FlipXRPC", RpcTarget.AllBuffered, Axis);
+        }
+    }
+
+    [PunRPC]
+    void FlipXRPC(float axis)
+    {
+        SR.flipX = axis == -1;
+    }
+
+}
